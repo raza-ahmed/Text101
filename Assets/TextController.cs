d45aafc7ded3c778f3ee2fe9c681256c2a1ba5ae
Ahmed Raza <raza2393@gmail.com>
2020-04-18 21:11:26 +0530

    Formatted code Text 1010

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 public class TextController : MonoBehaviour
 {
     public Text text;
-    private  enum States {bottom, horse, helicopter, person, helipad, pond, forest, location};
+    private  enum States {bottom, horse, helicopter, person, helipad, pond,
+                          forest, location};
     private States myState;
 
     // Start is called before the fir st frame update
@@ -32,21 +33,13 @@ public class TextController : MonoBehaviour
         myState = States.bottom;
         text.text = "You are at the bottom of a mountain and you want to climb it. " +
                         "You have a mountain horse, a helicopter and a person.\n\n" +
-                        "Press M to ride the horse, H to enter in the helicopter and P for asking a person";
+                        "Press M to ride the horse, H to enter in the helicopter "+
+                        "and P for asking a person";
 
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            State_horse();
-        }
-        else if (Input.GetKeyDown(KeyCode.H))
-        {
-            State_helicopter();
-
-        }
-        else if (Input.GetKeyDown(KeyCode.P))
-        {
-            State_person();
-        }
+        if (Input.GetKeyDown(KeyCode.M))            {State_horse();}
+        else if (Input.GetKeyDown(KeyCode.H))       {State_helicopter();}
+        else if (Input.GetKeyDown(KeyCode.P)) { State_person(); }
+        
     }
     void State_horse()
     {
@@ -54,10 +47,7 @@ public class TextController : MonoBehaviour
         text.text = "Horse gets tired and its freezing cold at the peak. " +
                     "No easy access of the grass for the horsy\n\n" +
                     "Press Return to go back";
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            myState = States.bottom;
-        }
+        if (Input.GetKeyDown(KeyCode.Return))       {myState = States.bottom;}
     }
     void State_person()
     {
@@ -65,29 +55,20 @@ public class TextController : MonoBehaviour
         text.text = "Two people can easily be caught. " +
                     "There are many spies who may broadcast your escape!\n\n" +
                     "Press Return to go back";
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            myState = States.bottom;
-        }
+        if (Input.GetKeyDown(KeyCode.Return))       {myState = States.bottom;}
     }
     void State_helicopter()
     {
         myState = States.helicopter;
         text.text = "Good choice! Pilot is skilled in flying in the moutains. " +
                     "A pro prison escaper.\n\n"+
-                    "Press D for Pond, F for Forest, A to land on Helipad";
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            myState = States.pond;
-        }
-        else if (Input.GetKeyDown(KeyCode.F))
-        {
-            myState = States.forest;
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            myState = States.helipad;
-        }
+                    "Press D for Pond, F for Forest, A to land on Helipad and " +
+                    "return to go back";
+
+        if (Input.GetKeyDown(KeyCode.D))            {myState = States.pond;}
+        else if (Input.GetKeyDown(KeyCode.F))       {myState = States.forest;}
+        else if (Input.GetKeyDown(KeyCode.A))       {myState = States.helipad;}
+        else if (Input.GetKeyDown(KeyCode.Return))  {myState = States.bottom;}
     }
     void State_pond()
     {
@@ -95,10 +76,7 @@ public class TextController : MonoBehaviour
         text.text = "Helicopter would drown on the pond! " +
                     "Find a better landing Place.\n\n" +
                     "Press Return to go back";
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            myState = States.helicopter;
-        }
+        if (Input.GetKeyDown(KeyCode.Return))       {myState = States.helicopter;}
     }
     void State_forest()
     {
@@ -106,10 +84,7 @@ public class TextController : MonoBehaviour
         text.text = "This forest is very dense. " +
                     "It might damage the helicopter.\n\n" +
                     "Press Return to go back";
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            myState = States.helicopter;
-        }
+        if (Input.GetKeyDown(KeyCode.Return))       {myState = States.helicopter;}
     }
 
     void State_helipad()
@@ -119,10 +94,7 @@ public class TextController : MonoBehaviour
                     "You Have reached an easy escape route.\n\n" +
                     "Press L and go to your location";
 
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            myState = States.location;
-        }
+        if (Input.GetKeyDown(KeyCode.L))            {myState = States.location;}
     }
     void State_location()
     {
@@ -130,9 +102,6 @@ public class TextController : MonoBehaviour
         text.text = "Congratulations!\n\n" +
                     "Press Space to play again";
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            myState = States.bottom;
-        }
+        if (Input.GetKeyDown(KeyCode.Space))        {myState = States.bottom;}
     }
 }
